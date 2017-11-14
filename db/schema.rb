cc7454147cd1c001c7223fc5bfc84d823d2cd62f
Alona <github email address>
2017-11-14 16:27:30 -0500

    picture likes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171114011552) do
+ActiveRecord::Schema.define(version: 20171114163042) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "liker_id"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20171114011552) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
+    t.string "avatar"
   end
 
 end
