c2e668a2661169da7ede96a069786c8e33695a57
Gonzodamus <Dick@TheDickWard.com>
2017-11-15 22:08:48 -0500

    Styling of root index and user index.  Fixed unlike.  Too long since last commit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIAJ7TWXKK4F5ISYHSA',                        # required
-    aws_secret_access_key: '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',                        # required
+    aws_access_key_id:     'AKIAITADSYZMPOI4WX7Q',                        # required
+    aws_secret_access_key: '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',                        # required
   }
   config.fog_directory  = 'loveappimages'                          # required
 end
