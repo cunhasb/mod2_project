33fe600aab089d112059b1cdbd5b791108003315
Gonzodamus <Dick@TheDickWard.com>
2017-11-16 09:18:10 -0500

    updated AWS keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIAITADSYZMPOI4WX7Q',                        # required
-    aws_secret_access_key: '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',                        # required
+    aws_access_key_id:     'AKIAIFRJ3QBX3EEFJ2KQ',                        # required
+    aws_secret_access_key: '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',                        # required
   }
   config.fog_directory  = 'loveappimages'                          # required
 end
